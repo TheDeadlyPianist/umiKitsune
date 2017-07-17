4cde3fd78504f6c2d8bc551a1401e08a157ef238
TheDeadlyPianist <duane221@yahoo.co.uk>
2017-07-17 12:01:38 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const dis = require("discord.js-commando");
+const umi = new dis.Client();
+
+umi.registrty.registerGroup("basic", "Basic Commands");
+
+umi.login("MzM2NDU5NjMxMDg4MTA3NTI1.DE4pqg.itzAIRssqArnDUYF25uKlvkXCZg")
